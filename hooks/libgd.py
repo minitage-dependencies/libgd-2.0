569ff4ea9e73f5ac681df0345f63da9eb2179899
kiorky <devnull@localhost>
2008-05-16 00:40:26 +0200

    0.4 normalization

--- hooks/libgd.py
+++ hooks/libgd.py
@@ -1,14 +1,6 @@
-import logging
 import os
-
-import zc.buildout
-
-log = logging.getLogger('libgd hook')
-
 def pre_configure(options, buildout):
     """Custom pre-make hook for building libgd"""
     #relibtoolize project"
     os.system("libtoolize --copy --force")
-
-
 
