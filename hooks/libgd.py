18ff453f53334d3e0cddc72ae16fa83a8d5ce134
kiorky <devnull@localhost>
2008-06-03 15:38:58 +0200

    ready for minitage v0.4

--- hooks/libgd.py
+++ hooks/libgd.py
@@ -2,5 +2,9 @@ import os
 def pre_configure(options, buildout):
     """Custom pre-make hook for building libgd"""
     #relibtoolize project"
-    os.system("libtoolize --copy --force")
+    os.system("""cd %s;
+              libtoolize --copy --force""" % (
+                  options['compile-directory']
+              )
+             )
 
